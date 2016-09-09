5e77657c1fdd14d76f760cac082fabe78a033cc4
BenYanez <yanezben1@gmail.com>
2016-09-09 10:17:05 -0700

    Add files via upload

---
+++ PYLESSON_03/RudeAI.py
@@ -0,0 +1,25 @@
+name = input("What is your name?")
+
+print(name , "?!! Why would anyone name a baby that?")
+
+old = input("How old are you,Ben?")
+
+print(old , "Oooooo!!! 14 is getting up there.")
+
+fun = input("What do you do for fun, Ben?")
+
+print(fun , "? I thought only nerds like to play soccer?")
+
+music = input("What kind of music do you like?")
+
+print(music , "? Boooo!!! I wouldn't wish the sound of rap on my worst enemy.")
+
+siblings = input("How many siblings do you have?")
+
+print(siblings , "? Wow, I hope the rest of your family is better looking.")
+
+grow = input("What do you want to be when you grow up?")
+
+print(grow , "? I think you'd have to be stronger to be a Pro_Soccer_Player.")
+
+print("So Ben, you're 14... You like to play soccer and listen to Rap... Good luck becoming a Pro_Soccer_Player. I'm only kidding Ben.")
