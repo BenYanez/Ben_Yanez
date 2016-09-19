39e086c81cd929dae916df9d58c4b6434f448f5c
BenYanez <yanezben1@gmail.com>
2016-09-19 09:58:17 -0700

    Add files via upload

---
+++ PYLesson_04/Lesson_04.py
@@ -0,0 +1,16 @@
+print("{:10} \t {:<10}".format("test", "newTest"))
+
+print("{:10} \t {:>10}".format("test", "newTest"))
+
+
+print("{:<10} \t {:>10}".format("test", "newTest"))
+
+print("{:_>10}{:_>10}".format("test", "newTest"))
+
+print("{:_^10}{:_^10}".format("test", "newTest"))
+
+
+
+
+print("{:10.4f}".format(13216.132165))
+
